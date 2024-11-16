fc753ab10c84264356023c192ff4b64ce091190a
abdulsalamamtech <abdulsalamamtech@gmail.com>
2024-11-16 13:24:52 +0100

    Add company partials

---
+++ resources/views/dashboard/partials/company/info.blade.php
@@ -0,0 +1,13 @@
+<div>
+
+    <div class="bg-white shadow-lg rounded-lg flex items-center gap-2 p-4 w-full dark:bg-slate-800 dark:text-gray-100">
+        <div class="md:p-2">
+            <i class="fa fa-truck bg-[#0828b9] p-2 rounded-full md:text-2xl text-white"></i>
+        </div>
+        <div class="sm:p-2 mb-4 md:mb-0">
+            <h2 class="md:text-xl font-bold mb-2">Amtech Oil & Gas</h2>
+            <p class="text-gray-700 dark:text-slate-400 mb-1">manager@amtechoilandgas.com</p>
+            <p class="text-gray-500 dark:text-gray-300">No. 54 Rimi, Gwarzo Road, Kano State</p>
+        </div>
+    </div>
+</div>
